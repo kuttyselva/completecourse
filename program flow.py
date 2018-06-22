b1925192327aa6401121e7db285e696633b0061b
kuttyselva <kuttyselva57@gmail.com>
2018-06-23 01:28:33 +0530

    program flow

---
+++ program flow.py
@@ -0,0 +1,15 @@
+#{x:y} the value y for spacing between values,x for the index of the format
+for i in range(1,12):
+    print("no {:2} squared is {:3} and cubed is {:5}".format(i,i**2,i**4))
+
+name=input("whats your name ?  ")
+age=int(input("whats your age {}  ".format(name)))
+print("hello "+name+" you are "+str(age)+" years old")
+if(age>=18):
+    {
+        print("and you are eligible for vote")
+    }
+else:{
+    print("and you are not eligible for vote")
+
+}
