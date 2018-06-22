f2f711435420ad87b90e1f6f0bb28cf9ce0dcdc6
kuttyselva <kuttyselva57@gmail.com>
2018-06-23 01:44:16 +0530

    program flow

--- program flow.py
+++ program flow.py
@@ -1,7 +1,10 @@
 #{x:y} the value y for spacing between values,x for the index of the format
+
+#for loop
 for i in range(1,12):
     print("no {:2} squared is {:3} and cubed is {:5}".format(i,i**2,i**4))
 
+# if case
 name=input("whats your name ?  ")
 age=int(input("whats your age {}  ".format(name)))
 print("hello "+name+" you are "+str(age)+" years old")
@@ -15,3 +18,22 @@ else:
     print("come back in %d years" %(18-age))
     print("thank you")
 
+#elif
+
+guess=int(input("guess a number between 1 to 10"))
+if guess<5:
+    print("guess higher")
+    guess=int(input())
+    if guess==5:
+        print("you passed this test")
+    else:
+        print("sorry try again ")
+elif guess>5:
+    print("please guess lower")
+    guess = int(input())
+    if guess==5:
+        print("you guessed correctly")
+    else:
+        print("sorry try again ")
+else:
+    print("guessed in first attempt")
